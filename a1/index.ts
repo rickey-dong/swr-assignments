8f396163c5bf85510870bd0e15af2f1b308ac534
Rickey Dong <rickeyd2004@gmail.com>
2021-01-24 19:15:31 -0500

    a1

--- a1/index.ts
+++ a1/index.ts
@@ -13,7 +13,7 @@ const isPrime = (num: number): boolean => { //helper isPrime function
     return true;
 }
 export const getPrimes = (array: number[]): number[] => {
-    return array.filter(isPrime); //filter the array based off which number is prime
+    return array.filter(n => isPrime(n)); //filter the array based off which number is prime
 };
 
 export const partition = (array: number[], belongToGroup1: (element: number) => boolean): { group1: number[]; group2: number[] } => {
